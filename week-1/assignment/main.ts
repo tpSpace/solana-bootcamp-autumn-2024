ed33bea3e41929e5479f0081affd0cc6acdee4aa
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-08-13 01:52:17 +0700

    update libs

--- week-1/assignment/main.ts
+++ week-1/assignment/main.ts
@@ -1,63 +1,15 @@
-import {
-  Connection,
-  Keypair,
-  PublicKey,
-  sendAndConfirmTransaction,
-  SystemProgram,
-  Transaction,
-} from "@solana/web3.js";
-import dotenv from "dotenv";
-import * as bs58 from "bs58";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-function createAccount(): Keypair {
-  const keypair = Keypair.generate();
-  console.log("Keypair generated:", keypair);
-  console.log("Public key:", keypair.publicKey.toBase58());
-  // Convert secret key to hex string
-  console.log("Private key:", bs58.default.encode(keypair.secretKey));
-  return keypair;
-}
+import { connection, payer } from "./lib/vars";
 
-function mintSol() {}
+(async () => {
+  console.log("Payer address:", payer.publicKey.toBase58());
 
-async function sendSol(sender: string, receiver: string, amount: number) {
-  const connection = new Connection(
-    "https://api.devnet.solana.com",
-    "confirmed"
+  // get the current balance of the `payer` account on chain
+  const currentBalance = await connection.getBalance(payer.publicKey);
+  console.log("Current balance of 'payer' (in lamports):", currentBalance);
+  console.log(
+    "Current balance of 'payer' (in SOL):",
+    currentBalance / LAMPORTS_PER_SOL
   );
-
-  const transaction = new Transaction().add(
-    SystemProgram.transfer({
-      fromPubkey: new PublicKey(sender),
-      toPubkey: new PublicKey(receiver),
-      lamports: amount,
-    })
-  );
-
-  await sendAndConfirmTransaction(connection, transaction, [keypair]);
-  // return txid;
-}
-
-function displayBalance(account: string): number {
-  const connection = new Connection(
-    "https://api.devnet.solana.com",
-    "confirmed"
-  );
-
-  connection.getBalance(new PublicKey(account)).then((balance) => {
-    console.log("Balance:", balance);
-    return balance;
-  });
-  return -1;
-}
-
-// ---------------------------------------------------------
-// read from dotenv file
-dotenv.config();
-const sender = process.env.PUBLIC_KEY as string;
-const testAccount = "GCuLmMtarn8BahRvp51LMPgK6vPcvbp4fH2mXqGdcfXK";
-displayBalance(sender);
-displayBalance(testAccount);
-
-const keypair = createAccount();
-console.log("Public key:", keypair.publicKey.toBase58());
+})();
