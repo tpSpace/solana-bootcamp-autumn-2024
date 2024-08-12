a99de95770c7571e02e1a2a4c36a9893a7af3a93
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2024-08-13 02:06:54 +0700

    A fail demo

--- week-1/assignment/README.md
+++ week-1/assignment/README.md
@@ -1,14 +1,16 @@
 # Assignment 1 - Interact with Solana network.
 
 ## Requirements
+
 - Write code to perform the following tasks:
-    - Create a new Solana account with a balance of some SOL.
-    - Transfer 0.1 SOL from the newly created account to `63EEC9FfGyksm7PkVC6z8uAmqozbQcTzbkWJNsgqjkFs` account.
-    - Close the newly created account.
+  - Create a new Solana account with a balance of some SOL.
+  - Transfer 0.1 SOL from the newly created account to `63EEC9FfGyksm7PkVC6z8uAmqozbQcTzbkWJNsgqjkFs` account.
+  - Close the newly created account.
 - All of the above steps must be completed in a single transaction.
 - Please include the signatures of your executed transactions run on devnet in your submission.
 
 ## Submission
+
 - Begin your work using the code found in the `/week-1/code` folder.
 - Ensure all the assignment code resides in the `/week-1/assignment` directory. The final submission deadline is `15/08/2024`.
 
--- week-1/assignment/main.ts
+++ week-1/assignment/main.ts
@@ -1,15 +1,61 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-
-import { connection, payer } from "./lib/vars";
+import solanaWeb3 from "@solana/web3.js";
+const {
+  Keypair,
+  Connection,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  SystemProgram,
+  sendAndConfirmTransaction,
+} = solanaWeb3;
 
 (async () => {
-  console.log("Payer address:", payer.publicKey.toBase58());
-
-  // get the current balance of the `payer` account on chain
-  const currentBalance = await connection.getBalance(payer.publicKey);
-  console.log("Current balance of 'payer' (in lamports):", currentBalance);
-  console.log(
-    "Current balance of 'payer' (in SOL):",
-    currentBalance / LAMPORTS_PER_SOL
+  // Connect to the Solana devnet
+  const connection = new Connection(
+    "https://api.devnet.solana.com",
+    "confirmed"
   );
+
+  // Create a new Solana account
+  const newAccount = Keypair.generate();
+  console.log("New account created:", newAccount.publicKey.toBase58());
+
+  // print out the balance of the new account
+  const balance = await connection.getBalance(newAccount.publicKey);
+  console.log("Balance of new account:", balance / LAMPORTS_PER_SOL);
+
+  // Airdrop some SOL to the new account
+  if (balance <= LAMPORTS_PER_SOL) {
+    console.log("Low balance, requesting an airdrop...");
+    await connection.requestAirdrop(newAccount.publicKey, LAMPORTS_PER_SOL);
+  }
+
+  // Recipient account
+  const recipientPublicKey = "63EEC9FfGyksm7PkVC6z8uAmqozbQcTzbkWJNsgqjkFs";
+
+  // Create a transaction
+  const transaction = new Transaction();
+
+  // Add transfer instruction
+  transaction.add(
+    SystemProgram.transfer({
+      fromPubkey: newAccount.publicKey,
+      toPubkey: new solanaWeb3.PublicKey(recipientPublicKey),
+      lamports: 0.1 * LAMPORTS_PER_SOL,
+    })
+  );
+
+  // Add close account instruction
+  transaction.add(
+    SystemProgram.closeAccount({
+      accountPubkey: newAccount.publicKey,
+      destinationPubkey: recipientPublicKey,
+      owner: newAccount.publicKey,
+    })
+  );
+
+  // Sign and send the transaction
+  const signature = await sendAndConfirmTransaction(connection, transaction, [
+    newAccount,
+  ]);
+  console.log("Transaction signature:", signature);
 })();
